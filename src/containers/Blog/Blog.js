4369abd4d8879c0e83aef32e680a9369ef294ad6
kamal11 <kyzx11@gmail.com>
2019-02-03 20:43:01 +0530

    routing enabled

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
+import NewPost from "../Blog/NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -11,15 +13,16 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/newpost">New Post</a>
+                <Link to="/new-post">New Post</Link>
               </li>
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
 
         {/* <section>
           <FullPost id={this.state.selectedPostId} />
