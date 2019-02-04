d4018396d7a8510336d3c1ce10bbfdfcfbe03136
kamal11 <kyzx11@gmail.com>
2019-02-04 17:36:19 +0530

    Added .active to navlink

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,7 +13,9 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
               </li>
               <li>
                 <NavLink to="/new-post">New Post</NavLink>
