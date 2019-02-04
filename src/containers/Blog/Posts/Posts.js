b54bc2e220e3b2f2aedce7c9d7b92adfaa89b53d
kamal11 <kyzx11@gmail.com>
2019-02-04 17:46:58 +0530

    key error resolved

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -35,9 +35,8 @@ export default class Posts extends Component {
     let posts = <p>Something went Wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map(post => (
-        <Link to={`/${post.id}`}>
+        <Link to={`/${post.id}`} key={post.id}>
           <Post
-            key={post.id}
             title={post.title}
             author={post.author}
             clicked={() => this.postSelectHanlder(post.id)}
